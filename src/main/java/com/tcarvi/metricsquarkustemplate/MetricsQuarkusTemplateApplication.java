a0184a7abc4366c9c80f86fb05937fe230725b16
TCARVI <tcarvi@me.com>
2021-09-27 00:26:50 -0300

    Implementação microprofile spec

---
+++ src/main/java/com/tcarvi/metricsquarkustemplate/MetricsQuarkusTemplateApplication.java
@@ -0,0 +1,12 @@
+package com.tcarvi.metricsquarkustemplate;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *  Inicialização da Aplicação
+ */
+@ApplicationPath("/aplicationName")
+public class MetricsQuarkusTemplateApplication extends Application {
+    // Scripts adicionais de inicialização do app.
+}
